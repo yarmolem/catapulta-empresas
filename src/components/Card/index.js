f89b79c1ee39012893d35f56dbeb6b01d59c535f
Cristian González <kazragore96@gmail.com>
2021-10-08 17:30:50 -0500

    First commit

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,14 +1,20 @@
-const Card = ({ icon, color = 'pink-600', number = '84', text = 'Usuarios activos' }) => {
-    return (
-      <div className={`max-w-44 max-h-44 bg-${color} flex flex-col text-white items-center 
-                      py-5 px-8 rounded-2xl mt-1 justify-center`}>
-        <div className="mt-1">
-          <img className='' src={icon} alt="" />
-        </div>
-        <p className='font-bold text-3xl mb-2'>{number}</p>
-        <p className='font-bold text-md tracking-widest text-center '>{text}</p>
+const Card = ({
+  icon,
+  number = '84',
+  color = 'pink-600',
+  text = 'Usuarios activos'
+}) => {
+  return (
+    <div
+      className={`max-w-44 max-h-44 bg-${color} flex flex-col text-white items-center py-5 px-8 rounded-2xl mt-1 justify-center`}
+    >
+      <div className="mt-1">
+        <img className="" src={icon} alt="" />
       </div>
-    )
-  }
-  
-  export default Card
+      <p className="font-bold text-3xl mb-2">{number}</p>
+      <p className="font-bold text-md tracking-widest text-center ">{text}</p>
+    </div>
+  )
+}
+
+export default Card
